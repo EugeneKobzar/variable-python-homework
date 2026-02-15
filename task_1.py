7d588cf8a34ba71a55e0caf6029cc706e1313c93
EugeneKobzar <kobvsh@gmail.com>
2026-02-15 22:50:00 +0200

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,11 @@
+number=int(input())
+a=number//10000
+b=number%10000//1000
+c=number%1000//100
+d=number%100//10
+e=number%10
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
---
+++ task_3.py
@@ -0,0 +1,4 @@
+import math
+r = float(input())
+l = 2 * math.pi * r
+print(l)
