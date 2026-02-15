851b201411c7952d5f1fdff4d7556107c55341a6
EugeneKobzar <kobvsh@gmail.com>
2026-02-16 00:18:49 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,9 +1,9 @@
 number = int(input('Enter your five-digit number: '))
-a = number//10000
-b = number%10000//1000
-c = number%1000//100
-d = number%100//10
-e = number%10
+a = number // 10000
+b = number % 10000 // 1000
+c = number % 1000 // 100
+d = number % 100 // 10
+e = number % 10
 print(a)
 print(b)
 print(c)
