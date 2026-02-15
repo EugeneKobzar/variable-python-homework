8589391ec0744d9286ee41de08f95175cc4f2e07
EugeneKobzar <kobvsh@gmail.com>
2026-02-16 00:00:03 +0200

    Delete task_2.py

--- task_2.py
+++
@@ -1,8 +0,0 @@
-import math
-
-a = float(input('inpute side a: '))
-b = float(input('inpute side b: '))
-c = float(input('inpute side c: '))
-p = (a + b + c) / 2
-s = math.sqrt(p * (p - a) * (p - b) * (p - c))
-print(s)
